e267e433bc71792af4a8281c00eade7714ad284f
rightlag <rightlag@gmail.com>
2017-04-12 14:14:20 -0400

    Add visitor test case

---
+++ tests/test_visitors.py
@@ -0,0 +1,15 @@
+import json
+import os
+import unittest
+
+from aptos.util import read
+from aptos.visitors import AvroSerializer
+
+
+class VisitorTestCase(unittest.TestCase):
+
+    def runTest(self):
+        specification = read(open(
+            os.path.join(os.path.dirname(__file__), 'schemas', 'petstore.json')))
+        v = specification.definitions['Pet'].accept(AvroSerializer())
+        print(json.dumps(v, indent=2))
