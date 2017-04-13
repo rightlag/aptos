5a90911c900ea36537de75643f433b0f2ae8c588
rightlag <rightlag@gmail.com>
2017-04-13 11:47:28 -0400

    Add __version__ to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from distutils.core import setup
 
+from aptos import __version__
+
 setup(
     name='aptos',
     author='Jason Walsh',
+    version=__version__,
     url='https://github.com/pennsignals/aptos'
 )
