5116611d5fb722e1bc37a99ab003409de7b1f7ec
rightlag <rightlag@gmail.com>
2017-05-15 11:55:12 -0400

    Add product schema / update test_visitors module

--- tests/test_visitors.py
+++ tests/test_visitors.py
@@ -2,16 +2,15 @@ import json
 import os
 import unittest
 
-from aptos.util import parse
+from aptos.util import Parser
 from aptos.visitors import RecordVisitor
 
 
 class VisitorTestCase(unittest.TestCase):
 
     def runTest(self):
-        specification = parse(open(
-            os.path.join(os.path.dirname(__file__), 'schemas',
-                         'petstore.json')))
-        Pet = specification.definitions['Pet'].accept(RecordVisitor())
-        self.assertEqual(len(Pet['fields']), 6)
-        print(json.dumps(Pet, indent=2))
+        specification = Parser.parse(
+            os.path.join(os.path.dirname(__file__), 'schemas', 'product'))
+        record = specification.accept(RecordVisitor())
+        self.assertEqual(len(record['fields']), 6)
+        print(json.dumps(record, indent=2))
