1ecb5b7acc09c45ee2876344d99c549b10724fab
rightlag <rightlag@gmail.com>
2017-05-15 13:03:22 -0400

    Update test_loader module to use product schema

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -1,23 +1,16 @@
 import os
 import unittest
 
-from aptos.util import parse
-from aptos.primitives import Array, Object
+from aptos.util import Parser
+from aptos.primitives import Array, Record
 
 
 class LoaderTestCase(unittest.TestCase):
 
     def runTest(self):
-        specification = parse(open(os.path.join(
-            os.path.dirname(__file__), 'schemas', 'petstore.json')))
+        record = Parser.parse(os.path.join(
+            os.path.dirname(__file__), 'schemas', 'product'))
 
-        self.assertEqual(len(specification.definitions), 6)
-        definitions = ['Order', 'Category', 'User', 'Tag', 'Pet',
-                       'ApiResponse']
-        for definition in definitions:
-            self.assertIn(definition, specification.definitions)
-
-        Pet = specification.definitions['Pet']
-        self.assertIsInstance(Pet.properties['category'], Object)
-        self.assertIsInstance(Pet.properties['tags'], Array)
-        self.assertIsInstance(Pet.properties['tags'].items, Object)
+        self.assertEqual(len(record.definitions), 1)
+        self.assertIsInstance(record.properties['warehouseLocation'], Record)
+        self.assertIsInstance(record.properties['tags'], Array)
