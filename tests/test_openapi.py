cfa4672cc18f993e07038a7cf1e02e9b34181135
rightlag <rightlag@gmail.com>
2017-05-25 07:29:39 -0400

    Add OpenAPI test case

---
+++ tests/test_openapi.py
@@ -0,0 +1,15 @@
+import json
+import os
+import unittest
+
+from aptos.models import OpenAPI, TypeVisitor
+from aptos.visitors import RecordVisitor
+
+
+class OpenAPITestCase(unittest.TestCase):
+
+    def runTest(self):
+        with open(os.path.join(os.path.dirname(__file__), 'schemas', 'swagger')) as fp:
+            instance = json.load(fp)
+        specification = OpenAPI.fromJson(instance)
+        specification.accept(TypeVisitor(instance))
