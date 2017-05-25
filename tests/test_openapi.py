aaad8cdcb0fbcd4b76433f21c7c017311d1dd4e6
rightlag <rightlag@gmail.com>
2017-05-25 07:51:03 -0400

    Update OpenAPI test case to use petstore spec

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -9,7 +9,10 @@ from aptos.visitors import RecordVisitor
 class OpenAPITestCase(unittest.TestCase):
 
     def runTest(self):
-        with open(os.path.join(os.path.dirname(__file__), 'schemas', 'swagger')) as fp:  # noqa: E501
+        with open(os.path.join(os.path.dirname(__file__), 'schemas', 'petstore')) as fp:  # noqa: E501
             instance = json.load(fp)
         specification = OpenAPI.fromJson(instance)
         specification.accept(TypeVisitor(instance))
+        record = specification.components['schemas']['Pet']
+        schema = record.accept(RecordVisitor())
+        print(json.dumps(schema, indent=2))
