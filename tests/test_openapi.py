f57f5f82441597fb32ffbcc746ee6a2b0af9e698
rightlag <rightlag@gmail.com>
2017-05-25 07:33:21 -0400

    Fix flake8 linting issues

--- tests/test_openapi.py
+++ tests/test_openapi.py
@@ -9,7 +9,7 @@ from aptos.visitors import RecordVisitor
 class OpenAPITestCase(unittest.TestCase):
 
     def runTest(self):
-        with open(os.path.join(os.path.dirname(__file__), 'schemas', 'swagger')) as fp:
+        with open(os.path.join(os.path.dirname(__file__), 'schemas', 'swagger')) as fp:  # noqa: E501
             instance = json.load(fp)
         specification = OpenAPI.fromJson(instance)
         specification.accept(TypeVisitor(instance))
